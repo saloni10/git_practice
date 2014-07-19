787353eae3865965fbba8f1788cc153a0820b816
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-19 22:28:45 +0530

    second commit

---
+++ ex1.py
@@ -0,0 +1,12 @@
+#Random file to practice git
+#Python , it is!
+# this comment is of no use 
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this. "
+print "this is fun"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "lets see what it does"
