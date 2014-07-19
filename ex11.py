3cebe8dc048f502b46307caf75b727cf8208b8ff
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-19 18:43:44 +0530

    first commit

---
+++ ex11.py
@@ -0,0 +1,11 @@
+#Random file to practice git
+
+ 
+print "How old are you?"
+age= raw_input()
+print "how tall are you?"
+height = raw_input()
+print "How much do you weigh?",
+weight= raw_input()
+
+print "So, you are %r old, %s tall and %r heavy." %(age, height, weight)
